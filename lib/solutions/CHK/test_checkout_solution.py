5dbe856bacabbde343be7e0e94129af1b9cfb271
sbx_user1051 <sbx_user1051@169.254.55.93>
2024-04-16 12:14:57 +0000

    Tue Apr 16 12:14:57 UTC 2024

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -29,6 +29,7 @@ def test_checkout_calculates_correct_total_prices():
     assert checkout("FFF") == 20
     assert checkout("AAABCCBBDAAFF") == 350
     assert checkout("AAABCCBBDAAFFF") == 350
+
     # TODO: stop mixing and matching skus at this point and just test the individual base prices and discounts
     # this should be ok because interaction between skus shouldn't differ between cases and above cases already
     # test the interaction. But with more time we would add cases that mix and match as many combinations as possible.
@@ -64,9 +65,6 @@ def test_checkout_calculates_correct_total_prices():
     assert checkout("Y") == 20
     assert checkout("Z") == 21
 
-
-
-
     ### Test illegal inputs return -1
     assert checkout("a") == -1
     assert checkout("b") == -1
@@ -80,48 +78,3 @@ def test_checkout_calculates_correct_total_prices():
     assert checkout("qwertyuiop") == -1
     assert checkout(1) == -1
     assert checkout(None) == -1
-
-
-
-
-# +------+-------+---------------------------------+
-# | Item | Price | Special offers                  |
-# +------+-------+---------------------------------+
-# | A    | 50    | 3A for 130, 5A for 200          |
-# | B    | 30    | 2B for 45                       |
-# | C    | 20    |                                 |
-# | D    | 15    |                                 |
-# | E    | 40    | 2E get one B free               |
-# | F    | 10    | 2F get one F free               |
-# | G    | 20    |                                 |
-# | H    | 10    | 5H for 45, 10H for 80           |
-# | I    | 35    |                                 |
-# | J    | 60    |                                 |
-# | K    | 70    | 2K for 120                      |
-# | L    | 90    |                                 |
-# | M    | 15    |                                 |
-# | N    | 40    | 3N get one M free               |
-# | O    | 10    |                                 |
-# | P    | 50    | 5P for 200                      |
-# | Q    | 30    | 3Q for 80                       |
-# | R    | 50    | 3R get one Q free               |
-# | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | U    | 40    | 3U get one U free               |
-# | V    | 50    | 2V for 90, 3V for 130           |
-# | W    | 20    |                                 |
-# | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# +------+-------+---------------------------------+
-
-
-
-
-# Base prices no change
-# K 80    | 2K for 150   -> 2K for 120
-# other discounts no change
-
-
-# new discounts
-# S T X Y Z all 3 for 45
