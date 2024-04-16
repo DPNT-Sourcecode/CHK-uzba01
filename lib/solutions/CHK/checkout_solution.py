f0e51818a4a26f7bd98514d88081af5abfb10bde
sbx_user1051 <sbx_user1051@169.254.57.197>
2024-04-16 11:35:40 +0000

    Tue Apr 16 11:35:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,27 +4,31 @@ from typing import Optional, List
 
 
 def checkout(skus):
-    skus_to_counts = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
+    skus_to_counts = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0}
 
-    skus_to_base_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+    skus_to_base_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
     skus_to_discounts = {
         "A": [
             {"discount_group_size": 5, "discounted_price": 200},
             {"discount_group_size": 3, "discounted_price": 130}
         ],
-        "B": [{"discount_group_size": 2, "discounted_price": 45}]
+        "B": [{"discount_group_size": 2, "discounted_price": 45}],
     }
    
-    try:
-        for sku in skus:
-            if sku in skus_to_counts:
-                skus_to_counts[sku] += 1
-            else:
-                return -1
-    except TypeError:
+    if skus:
+        try:
+            for sku in skus:
+                if sku in skus_to_counts:
+                    skus_to_counts[sku] += 1
+                else:
+                    return -1
+        except TypeError:
+            return -1
+    else:
         return -1
     
     skus_to_counts["B"] = max(0, skus_to_counts["B"] - skus_to_counts["E"] // 2)
+    skus_to_counts["F"] = max(0, skus_to_counts["F"] - skus_to_counts["F"] // 3)
 
     total_price = 0
     for sku, sku_count in skus_to_counts.items():
@@ -45,3 +49,4 @@ def calculate_total_price(sku_count: int, price: int, discounts: Optional[List])
             total += groups * discount["discounted_price"]
     total += sku_count * price
     return total
+
