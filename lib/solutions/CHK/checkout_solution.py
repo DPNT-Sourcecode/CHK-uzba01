ce112da209bc9a3d160299e6fab0e1565a7867f0
sbx_user1051 <sbx_user1051@169.254.55.93>
2024-04-16 11:58:22 +0000

    Tue Apr 16 11:58:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,11 @@ from typing import Optional, List
 def checkout(skus):
     skus_to_counts = {chr(i): 0 for i in range(ord('A'), ord('Z')+1)}
 
-    skus_to_base_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
+    skus_to_base_prices = {
+        "A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35, 
+        "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, 
+        "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50
+    }    
     skus_to_discounts = {
         "A": [
             {"discount_group_size": 5, "discounted_price": 200},
@@ -19,6 +23,12 @@ def checkout(skus):
         ],
         "K": [{"discount_group_size": 2, "discounted_price": 150}],
         "P": [{"discount_group_size": 5, "discounted_price": 200}],
+        "Q": [{"discount_group_size": 3, "discounted_price": 80}],
+        "V": [
+            {"discount_group_size": 3, "discounted_price": 130},
+            {"discount_group_size": 2, "discounted_price": 90}
+        ]
+
     }
    
 
@@ -35,6 +45,8 @@ def checkout(skus):
     skus_to_counts["B"] = max(0, skus_to_counts["B"] - skus_to_counts["E"] // 2)
     skus_to_counts["F"] = max(0, skus_to_counts["F"] - skus_to_counts["F"] // 3)
     skus_to_counts["M"] = max(0, skus_to_counts["M"] - skus_to_counts["N"] // 3)
+    skus_to_counts["Q"] = max(0, skus_to_counts["Q"] - skus_to_counts["R"] // 3)
+    skus_to_counts["U"] = max(0, skus_to_counts["U"] - skus_to_counts["U"] // 4)
 
     total_price = 0
     for sku, sku_count in skus_to_counts.items():
@@ -55,6 +67,7 @@ def calculate_total_price(sku_count: int, price: int, discounts: Optional[List])
             total += groups * discount["discounted_price"]
     total += sku_count * price
     return total
+
 
 
 
