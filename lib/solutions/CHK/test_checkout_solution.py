d29c8a83fc0f708066b6ba3359d04c151d3f2765
sbx_user1051 <sbx_user1051@169.254.55.93>
2024-04-16 12:04:22 +0000

    Tue Apr 16 12:04:22 UTC 2024

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -29,7 +29,37 @@ def test_checkout_calculates_correct_total_prices():
     assert checkout("FFF") == 20
     assert checkout("AAABCCBBDAAFF") == 350
     assert checkout("AAABCCBBDAAFFF") == 350
+    # TODO: stop mixing and matching skus at this point and just test the individual base prices and discounts
+    # this should be ok because interaction between skus shouldn't differ between cases and above cases already
+    # test the interaction. But with more time we would add cases that mix and match as many combinations as possible.
     assert checkout("") == 0
+    assert checkout("G") == 20
+    assert checkout("H") == 10
+    assert checkout("HHHHH") == 45
+    assert checkout("HHHHHHHHHH") == 80
+    assert checkout("I") == 35
+    assert checkout("J") == 60
+    assert checkout("K") == 80
+    assert checkout("KK") == 150
+    assert checkout("L") == 90
+    assert checkout("M") == 15
+    assert checkout("N") == 40
+    assert checkout("NNNM") == 120
+    assert checkout("O") == 10
+    assert checkout("P") == 50
+    assert checkout("PPPPP") == 200
+    assert checkout("Q") == 30
+    assert checkout("QQQ") == 80
+    assert checkout("RRRQ") == 150
+    assert checkout("R") == 50
+    assert checkout("S") == 30
+    assert checkout("T") == 20
+    assert checkout("U") == 40
+    assert checkout("UUUU") == 120
+    assert checkout("V") == 50
+    assert checkout("VV") == 90
+    assert checkout("VVV") == 130
+
 
 
 
@@ -46,3 +76,4 @@ def test_checkout_calculates_correct_total_prices():
     assert checkout("qwertyuiop") == -1
     assert checkout(1) == -1
     assert checkout(None) == -1
+
