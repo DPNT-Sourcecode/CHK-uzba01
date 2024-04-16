36aa295266c30a7335752b9b86dcdff977a701c6
sbx_user1051 <sbx_user1051@169.254.35.45>
2024-04-16 08:47:08 +0000

    Tue Apr 16 08:47:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,31 +1,69 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    skus_to_counts = {"A": 0, "B": 0, "C": 0, "D": 0}
+# def checkout(skus):
+#     skus_to_counts = {"A": 0, "B": 0, "C": 0, "D": 0}
 
-    skus_to_prices = {"A": 50, "B": 30, "C": 20, "D": 15}
-    skus_to_discounts = {"A": (3, 130), "B": (2, 45)}
+#     skus_to_prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+#     skus_to_discounts = {"A": (3, 130), "B": (2, 45)}
+
+#     try:
+#         for sku in skus:
+#             if sku in skus_to_counts:
+#                 skus_to_counts[sku] += 1
+#             else:
+#                 return -1 # We could alternatively raise ValueError(f"Invalid SKU {sku}")
+#     except TypeError:
+#         return -1
+
+#     total_price = 0
+#     for sku, count in skus_to_counts.items():
+#         total_price += calculate_total_price(count, skus_to_prices[sku], skus_to_discounts.get(sku))
+
+#     return total_price
+
+
+# def calculate_total_price(count, price, discount):
+#     if discount:
+#         discount_group_size, discounted_group_price = discount
+#         discount_groups_count, non_discounted_items = divmod(count, discount_group_size)
+#         return discount_groups_count * discounted_group_price + non_discounted_items * price
+#     else:
+#         return count * price
+
+
+
+def checkout(skus):
+    skus_to_counts = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
+
+    skus_to_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+    skus_to_discounts = {"A": [(5, 200), (3, 130)], "B": [(2, 45)], "E": [(2, "B")]}
 
     try:
         for sku in skus:
             if sku in skus_to_counts:
                 skus_to_counts[sku] += 1
             else:
-                return -1 # We could alternatively raise ValueError(f"Invalid SKU {sku}")
+                return -1
     except TypeError:
         return -1
 
     total_price = 0
     for sku, count in skus_to_counts.items():
-        total_price += calculate_total_price(count, skus_to_prices[sku], skus_to_discounts.get(sku))
+        total_price += calculate_total_price(count, skus_to_prices[sku], skus_to_discounts.get(sku), skus_to_counts)
 
     return total_price
 
 
-def calculate_total_price(count, price, discount):
-    if discount:
-        discount_group_size, discounted_group_price = discount
-        discount_groups_count, non_discounted_items = divmod(count, discount_group_size)
-        return discount_groups_count * discounted_group_price + non_discounted_items * price
-    else:
-        return count * price
+def calculate_total_price(count, price, discounts, skus_to_counts):
+    total = 0
+    if discounts:
+        for discount in sorted(discounts, reverse=True):
+            if isinstance(discount[1], int):
+                discount_group_size, discounted_group_price = discount
+                groups, count = divmod(count, discount_group_size)
+                total += groups * discounted_group_price
+            else:  # Special offer: get other SKU for free
+                free_sku = discount[1]
+                skus_to_counts[free_sku] = max(0, skus_to_counts[free_sku] - count // discount[0])
+    total += count * price
+    return total
