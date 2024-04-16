67d0efce336b3e879de6611fcbbb2c314675c361
sbx_user1051 <sbx_user1051@169.254.61.245>
2024-04-16 08:10:10 +0000

    Tue Apr 16 08:10:10 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,30 +1,3 @@
-
-def test_checkout_calculates_correct_total_prices():
-    assert checkout("A") == 50
-    assert checkout("B") == 30
-    assert checkout("C") == 20
-    assert checkout("D") == 15
-    assert checkout("AA") == 100
-    assert checkout("BB") == 45
-    assert checkout("CC") == 40
-    assert checkout("DD") == 30
-    assert checkout("AAA") == 130
-    assert checkout("BBB") == 75
-    assert checkout("CCC") == 60
-    assert checkout("DDD") == 45
-    assert checkout("ABCD") == 115
-    assert checkout("ABCDABCD") == 230
-    assert checkout("AAABBBCCCDDD") == 290
-    assert checkout("AAABBBCCCDDDAAABBBCCCDDD") == 580
-    assert checkout("AAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDD") == 870
-    assert checkout("AAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDD") == 1160
-    assert checkout("AAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDD") == 1450
-    assert checkout("AAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDDAAABBBCCCDDD") == 1740
-
-
-
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -58,7 +31,4 @@ def checkout(skus):
     d_total_price = len(d_sku_items) * 15
 
     return a_total_price + b_total_price + c_total_price + d_total_price
-
-
-
 
